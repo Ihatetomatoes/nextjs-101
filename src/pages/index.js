50a477d49d55cdab1e03d52d64f6c4af26a99d2a
Petr Tichy <peter@ihatetomatoes.net>
2020-11-11 16:13:44 +1100

    seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { getAllPosts } from "../../lib/api";
 import { Author, Header, Hero, Layout } from "../components";
-import { author, ogImage, siteName } from "../components/Meta";
+import { author, ogImage, siteDescription, siteName } from "../components/Meta";
 
 export default function Home({ allPosts }) {
   return (
@@ -10,11 +10,22 @@ export default function Home({ allPosts }) {
         <title>{`${siteName} by ${author}`}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta
+          property="og:url"
+          content="https://ihatetomatoes-nextjs-101.vercel.app/"
+          key="url"
+        />
+        <meta property="og:type" content="website" key="website" />
+        <meta
           property="og:title"
           content={`${siteName} by ${author}`}
           key="title"
         />
         <meta property="og:image" content={ogImage} key="image" />
+        <meta
+          property="og:description"
+          content={siteDescription}
+          key="description"
+        />
       </Head>
       <main className="bg-white p-4 sm:p-8 min-h-full col-span-8 col-start-2">
         <Header />
