e49d67ebae7c578331c87f8a251f0302f4381153
Petr Tichy <peter@ihatetomatoes.net>
2020-12-03 18:37:32 +1100

    collapsible modules

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -1,65 +1,104 @@
 import { groupBy } from "@utils/utils";
 import classNames from "classnames";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { withRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { Pill } from "./";
 
-const ProgressMarker = ({ isCurrent, isCompleted }) => {
+const ProgressMarker = ({ isCompleted }) => {
   const markerClass = classNames({
     "bg-gray-200 text-white rounded-full inline-block w-2 h-2 self-center mr-2 transition-colors duration-200": true,
-    "bg-completed": isCompleted || isCurrent,
+    "bg-completed": isCompleted,
   });
   return <span className={markerClass}>&nbsp;</span>;
 };
 
 const UnitItem = ({ unit: { slug, title, duration }, progress, query }) => {
-  const listItemClass = classNames({
-    "text-sm": true,
-    "font-bold": slug === query.slug,
+  const isCompleted = progress.find((item) => item.path === slug);
+  const linkClass = classNames({
+    "p-1 px-2 flex place-self-stretch rounded-md": true,
+    "bg-green-100": slug === query.slug,
   });
+
+  const pillClass = classNames({
+    "bg-green-100": slug === query.slug,
+  });
+
   return (
-    <li className={listItemClass}>
+    <li className="text-sm">
       <Link href={`/post/${slug}`}>
-        <a className="p-1 px-0 flex place-self-stretch">
-          <ProgressMarker
-            isCurrent={slug === query.slug}
-            isCompleted={progress.find((item) => item.path === slug)}
-          />
+        <a className={linkClass}>
+          <ProgressMarker isCompleted={isCompleted} />
           <span className="lg:flex-1">{title}</span>
-          <Pill text={duration} />
+          <Pill text={duration} className={pillClass} />
         </a>
       </Link>
     </li>
   );
 };
 
-const ModuleItem = ({ module }) => <h3 className="font-bold py-2">{module}</h3>;
+const ModuleItem = ({ module, units, query, progress }) => {
+  const isCurrentModule = units.find((u) => u.slug === query.slug);
+  const [isOpen, setIsOpen] = useState(isCurrentModule);
+  const toggleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <h3 className="font-bold p-2" onClick={toggleOpen}>
+        {module}
+      </h3>
+      <AnimatePresence>
+        {isOpen && units && (
+          <motion.div
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto", overflow: "hidden" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.3, ease: [0.5, 0.7, 0.4, 1] }}
+          >
+            <ul>
+              {units.map((unit) => (
+                <UnitItem
+                  key={unit.slug}
+                  unit={unit}
+                  query={query}
+                  progress={progress}
+                />
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
 
 const Modules = ({ units = [], router: { query }, progress }) => {
   const modules = groupBy(units, "module");
   return (
-    <div>
-      {modules.map(({ name, units }) => {
-        return (
-          <div key={name}>
-            <ModuleItem module={name} />
-            {units && (
-              <ul>
-                {units.map((unit) => (
-                  <UnitItem
-                    key={unit.slug}
-                    unit={unit}
-                    query={query}
-                    progress={progress}
-                  />
-                ))}
-              </ul>
-            )}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {modules.length ? (
+        modules.map(({ name, units }) => {
+          return (
+            <ModuleItem
+              key={name}
+              module={name}
+              units={units}
+              query={query}
+              progress={progress}
+            />
+          );
+        })
+      ) : (
+        <p className="text-sm text-center">
+          There are not units in this course.
+        </p>
+      )}
+    </>
   );
 };
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,26 +6,28 @@ import { CourseProgress, Modules, ResetProgressBtn } from "./";
 
 const Sidebar = ({ units, progress, isSticky }) => {
   const sidebarClass = classNames({
-    "p-4 bg-white border border-gray-200 md:border-0 m-4 lg:m-0 lg:ml-4": true,
+    "px-2 py-4 bg-white border border-gray-200 md:border-0 m-4 lg:m-0 lg:ml-4": true,
     "sticky top-0": isSticky,
   });
 
   return (
     <div className={sidebarClass}>
       <div className="py-4">
-        <FeedbackFish projectId={process.env.NEXT_PUBLIC_FEEDBACK_FISH_ID}>
-          <button
-            className="bold py-2 px-4 mb-4 w-full inline-block text-center bg-gray-200 hover:bg-red-500 transition-colors duration-200 text-white rounded-sm"
-            title="Leave a short review, question or feedback. Thank you!"
-          >
-            Give me feedback
-          </button>
-        </FeedbackFish>
-        <CourseProgress
-          units={units}
-          progress={progress}
-          className="hidden md:block"
-        />
+        <div className="px-2">
+          <FeedbackFish projectId={process.env.NEXT_PUBLIC_FEEDBACK_FISH_ID}>
+            <button
+              className="bold py-2 px-4 mb-4 w-full inline-block text-center bg-gray-200 hover:bg-red-500 transition-colors duration-200 text-white rounded-sm"
+              title="Leave a short review, question or feedback. Thank you!"
+            >
+              Give me feedback
+            </button>
+          </FeedbackFish>
+          <CourseProgress
+            units={units}
+            progress={progress}
+            className="hidden md:block"
+          />
+        </div>
         <div className="md:p-0">
           <Modules units={units} progress={progress} />
           <Link href="/">
